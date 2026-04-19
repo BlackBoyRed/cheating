f29ad20b9503b843d9281c7e7d94864d00d51eed
BlackBoyRedHeart <151723486+BlackBoyRed@users.noreply.github.com>
2026-04-19 12:56:03 +0530

    Update p7_Verify_Digital_signatures_Validate_transactionns.py

--- IBT PE/p7_Verify_Digital_signatures_Validate_transactionns.py
+++ IBT PE/p7_Verify_Digital_signatures_Validate_transactionns.py
@@ -1,34 +1,22 @@
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
+import hashlib
 
-# Generate keys (sender)
-private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-public_key = private_key.public_key()
+# Define keys
+private_key = "my_private_key"
+public_key = "my_public_key"
 
-# Transaction data
-transaction = b"Alice pays Bob 10 coins"
+# Message
+message = "Kate pays Johnny 10 BTC"
 
-# Sign transaction
-signature = private_key.sign(
-    transaction,
-    padding.PSS(
-        mgf=padding.MGF1(hashes.SHA256()),
-        salt_length=padding.PSS.MAX_LENGTH
-    ),
-    hashes.SHA256()
-)
+# Generate Digital Signature using private key
+signature = hashlib.sha256((message + private_key).encode()).hexdigest()
 
-# Verify transaction
-try:
-    public_key.verify(
-        signature,
-        transaction,
-        padding.PSS(
-            mgf=padding.MGF1(hashes.SHA256()),
-            salt_length=padding.PSS.MAX_LENGTH
-        ),
-        hashes.SHA256()
-    )
-    print("Transaction is VALID")
-except:
-    print("Transaction is INVALID")
+print("Message:", message)
+print("Digital Signature:", signature)
+
+# Verify Digital Signature using public key (simulation)
+verify_signature = hashlib.sha256((message + private_key).encode()).hexdigest()
+
+if signature == verify_signature:
+    print("Signature is VALID")
+else:
+    print("Signature is INVALID")
